328a5be9b824d87ede6dbb68d9c52a304cca2f46
前嶋広樹 <mesm0826@gmail.com>
2016-03-19 12:58:39 +0900

    ログイン画面のテスト(前嶋)

---
+++ WebContent/LoginResult.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=UTF-8>
+<title>LoginResult</title>
+</head>
+<body>
+	ログイン完了！
+</body>
+</html>
---
+++ src/servlet/login.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet(name="servlet.login", urlPatterns ={"/login"})
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charaset=UTF-8");
+		request.setCharacterEncoding("utf-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/LoginResult.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
